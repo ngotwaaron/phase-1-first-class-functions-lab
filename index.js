7dc289d79dde64b34d263a309da04462b5f78730
ngotwaaron <ngotwaaron@gmail.com>
2024-03-25 14:36:52 +0300

    complete javascript advanced first class functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function (drivers) {
+  const twoDrivers = drivers.slice(0, 2);
+  return twoDrivers;
+};
+const returnLastTwoDrivers = function (drivers) {
+  const lastDrivers = drivers.slice(-2);
+  return lastDrivers;
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(interger) {
+  const fareMultiplier = fare => fare * interger;
+  return fareMultiplier;
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers, selectingDrivers) {
+  return selectingDrivers(drivers);
+}
+
